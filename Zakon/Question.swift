5ba482b3ffc07f358ef8127689524a75c67ddefb
Assel-Salavatova <assel@assels-iMac.local>
2016-12-25 03:09:45 -0800

    question class

---
+++ Zakon/Question.swift
@@ -0,0 +1,38 @@
+//
+//  Question.swift
+//  Zakon
+//
+//  Created by assel on 12/25/16.
+//  Copyright © 2016 AA. All rights reserved.
+//
+
+import UIKit
+
+class Question: UIViewController {
+
+    var question : String!
+    var variants : [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

400ecc358f4e6102b877489ebefe15518fcf0736
wormhole23 <user@Users-iMac.local>
2016-12-28 21:28:49 +0600

    First task

--- Zakon/ViewController.swift
+++ Zakon/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var questions: [Question]?
+    var questionIndex = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func populateQuestions() {
+        // Заполняет массив questions вопросами с базы,
+        // но пока будем вбивать значения вручную
+        
+    }
+    
+    func showNextQuestion() {
+        // Выводит на view следующий вопрос по индексу
+        
+    }
+    
+    func showPrevQuestion(){
+        // Выводит на view предыдущий вопрос по индексу
+        
+    }
+    
     @IBAction func playButtonPressed(_ sender: UIButton) {
         self.performSegue(withIdentifier: "toQuestionsVC", sender: nil)
     }

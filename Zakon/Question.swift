43b9829239742dd6cc4a3a636ed4ab5ac6449819
wormhole23 <user@Users-iMac.local>
2016-12-28 20:54:35 +0600

    Question class updated

--- Zakon/Question.swift
+++ Zakon/Question.swift
@@ -2,37 +2,18 @@
 //  Question.swift
 //  Zakon
 //
-//  Created by assel on 12/25/16.
+//  Created by User on 12/28/16.
 //  Copyright © 2016 AA. All rights reserved.
 //
 
 import UIKit
 
-class Question: UIViewController {
+class Question: NSObject {
 
-    var question : String!
-    var variants : [String] = []
+    var question: String?
+    var varinats: [String]?
+    var rightAnswer: String?
+    var userAnswer: String?
+    var isRight: Bool = false
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

bed86f18af8e7dd5aba6b55cfbe2d862627c874a
Assel-Salavatova <assel@assels-iMac.local>
2017-01-05 08:13:52 -0800

    show question

--- Zakon/QuestionsViewController.swift
+++ Zakon/QuestionsViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 
+    @IBOutlet weak var questionText: UITextView!
+    var questions: [Question]?
+    var questionIndex = -1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        populateQuestions()
         // Do any additional setup after loading the view.
     }
 
@@ -25,6 +28,37 @@ class QuestionsViewController: UIViewController {
     @IBAction func nextButtonPressed(_ sender: UIButton) {
         self.performSegue(withIdentifier: "toResultVC", sender: nil)
     }
+    
+    func populateQuestions() {
+        // Заполняет массив questions вопросами с базы,
+        // но пока будем вбивать значения вручную
+        let q1 = Question()
+        q1.question = "Вы знаете конституцию РК?"
+        q1.varinats = ["Да", "Нет", "Может быть"]
+        q1.rightAnswer = "Может быть"
+        
+        let q2 = Question()
+        q2.question = "Легальна ли марихуана в РК?"
+        q2.varinats = ["Да", "Нет"]
+        q2.rightAnswer = "Нет"
+        
+        questions?.append(q1)
+        questions?.append(q2)
+        
+        showNextQuestion()
+    }
+    
+    func showNextQuestion() {
+        // Выводит на view следующий вопрос по индексу
+        questionText.text = questions?[0].question
+        print(questions?[0].question)
+    }
+    
+    func showPrevQuestion(){
+        // Выводит на view предыдущий вопрос по индексу
+        
+    }
+
 
     /*
     // MARK: - Navigation

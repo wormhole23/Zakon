60362cab5d31e0b8d3dce280d27658b8332e3e83
wormhole23 <user@Users-iMac.local>
2017-01-05 20:39:12 +0600

    Added db simulation

--- Zakon/ViewController.swift
+++ Zakon/ViewController.swift
@@ -26,7 +26,20 @@ class ViewController: UIViewController {
     func populateQuestions() {
         // Заполняет массив questions вопросами с базы,
         // но пока будем вбивать значения вручную
+        let q1 = Question()
+        q1.question = "Вы знаете конституцию РК?"
+        q1.varinats = ["Да", "Нет", "Может быть"]
+        q1.rightAnswer = "Может быть"
         
+        let q2 = Question()
+        q2.question = "Легальна ли марихуана в РК?"
+        q2.varinats = ["Да", "Нет"]
+        q2.rightAnswer = "Нет"
+        
+        questions?.append(q1)
+        questions?.append(q2)
+        
+        showNextQuestion()
     }
     
     func showNextQuestion() {
